0533647cff341f93ec0af634953116e788a73d95
MuthuM03 <muthu.m3w@gmail.com>
2023-07-18 15:46:11 +0530

    url-shortner frontend file

--- server/app.js
+++ server/app.js
@@ -1,11 +1,27 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 
+const DB_URL = `mongodb://localhost:27017/URL_Shorter`;
 const app = express();
 
-require('./routes')(app)
+require("./routes")(app);
+
+app.set("view engine", "ejs");
 
 app.use((req, res, next) => {
-    res.send("Hello world")
-})
+    res.send("Hello world");
+});
 
-app.listen(1000)
+const port = process.env.PORT | 2000;
+
+mongoose
+    .connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('DB Connected');
+        app.listen(port, () => {
+            console.log(`Server running in ${port} port`);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
